nitrogen-project/android_packages_apps_NitrogenSettings

ButtonBacklight: Rework for bacon with CustomSeekBar

src/com/nitrogen/settings/fragments/ButtonSettings.java
@@ -31,6 +31,7 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
 import com.android.settings.SettingsPreferenceFragment;
+import com.nitrogen.settings.preferences.CustomSeekBarPreference;
 
 public class ButtonSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
@@ -84,7 +85,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private ListPreference mBacklightTimeout;
     private ListPreference mKillAppLongpressTimeout;
     private SwitchPreference mEnableHwKeys;
-    private SwitchPreference mButtonBrightness;
+    private CustomSeekBarPreference mButtonBrightness;
 
     private Handler mHandler;
 
@@ -120,7 +121,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
                 (SwitchPreference) findPreference(KEY_ENABLE_HW_KEYS);
 
         mButtonBrightness =
-                (SwitchPreference) findPreference(KEY_BUTTON_BRIGHTNESS);
+                (CustomSeekBarPreference) findPreference(KEY_BUTTON_BRIGHTNESS);
 
         mHandler = new Handler();
 
@@ -184,9 +185,11 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
             }
 
             if (mButtonBrightness != null) {
-        	mButtonBrightness.setChecked((Settings.System.getInt(getContentResolver(),
-            	    Settings.System.BUTTON_BRIGHTNESS, 1) == 1));
-        	mButtonBrightness.setOnPreferenceChangeListener(this);
+                int ButtonBrightness = Settings.System.getInt(resolver,
+                                Settings.System.BUTTON_BRIGHTNESS, 255);
+                mButtonBrightness.setValue(ButtonBrightness / 1);
+                mButtonBrightness.setOnPreferenceChangeListener(this);
+
             }
         } else {
             prefScreen.removePreference(mBacklightTimeout);
@@ -267,9 +270,9 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
                     Settings.System.ENABLE_HW_KEYS, value ? 1 : 0);
             return true;
         } else if (preference == mButtonBrightness) {
-            boolean value = (Boolean) newValue;
+            int value = (Integer) newValue;
             Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.BUTTON_BRIGHTNESS, value ? 1 : 0);
+                    Settings.System.BUTTON_BRIGHTNESS, value * 1);
             return true;
         }
         return false;

Suicide-Squad-OMS/platform_packages_apps_Settings

src/com/android/settings/zephyr/ButtonSettings.java
@@ -23,6 +23,7 @@ import android.content.res.Resources;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.UserHandle;
+import android.os.Handler;
 import android.os.Vibrator;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.ListPreference;
@@ -39,6 +40,7 @@ import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.internal.utils.du.ActionConstants;
 import com.android.internal.utils.du.DUActionUtils;
 import com.android.settings.du.nav.ActionFragment;
+import com.android.settings.zephyr.CustomSeekBarPreference;
 
 public class ButtonSettings extends ActionFragment implements OnPreferenceChangeListener {
 
@@ -63,7 +65,9 @@ public class ButtonSettings extends ActionFragment implements OnPreferenceChange
 
     private SwitchPreference mHwKeyDisable;
     private ListPreference mBacklightTimeout;
-    private SwitchPreference mButtonBrightness;
+    private CustomSeekBarPreference mButtonBrightness;
+
+    private Handler mHandler;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -113,7 +117,9 @@ public class ButtonSettings extends ActionFragment implements OnPreferenceChange
         mBacklightTimeout =
                 (ListPreference) findPreference(KEY_BACKLIGHT_TIMEOUT);
         mButtonBrightness =
-                (SwitchPreference) findPreference(KEY_BUTTON_BRIGHTNESS);
+                (CustomSeekBarPreference) findPreference(KEY_BUTTON_BRIGHTNESS);
+
+        mHandler = new Handler();
 
         // back key
         if (!hasBackKey) {
@@ -151,9 +157,10 @@ public class ButtonSettings extends ActionFragment implements OnPreferenceChange
             }
 
             if (mButtonBrightness != null) {
-        	mButtonBrightness.setChecked((Settings.System.getInt(getContentResolver(),
-            	    Settings.System.BUTTON_BRIGHTNESS, 1) == 1));
-        	mButtonBrightness.setOnPreferenceChangeListener(this);
+                int ButtonBrightness = Settings.System.getInt(getContentResolver(),
+                                Settings.System.BUTTON_BRIGHTNESS, 255);
+                mButtonBrightness.setValue(ButtonBrightness / 1);
+                mButtonBrightness.setOnPreferenceChangeListener(this);
             }
         } else {
             prefScreen.removePreference(mBacklightTimeout);
@@ -196,9 +203,9 @@ public class ButtonSettings extends ActionFragment implements OnPreferenceChange
                     .setSummary(mBacklightTimeout.getEntries()[BacklightTimeoutIndex]);
             return true;
         } else if (preference == mButtonBrightness) {
-            boolean value = (Boolean) newValue;
+            int value = (Integer) newValue;
             Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.BUTTON_BRIGHTNESS, value ? 1 : 0);
+                    Settings.System.BUTTON_BRIGHTNESS, value * 1);
             return true;
         }
         return false;